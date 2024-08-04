87970f3119de848a4b83adfb868d4dcabb3bae16
Sabitica <sarahlecornec123+1@gmail.com>
2024-08-04 09:09:17 +0100

    extending library, testing and purging unused css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,37 @@
 const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+// for future reference, the above gul-purgecss changed the index.css file size 
+// from 2572 lines to 385 WOW
+
+// function buildStyles() {
+//     return src('sabitica/**/*.scss')
+//         .pipe(sass())
+//         .pipe(purgecss({content: ['*.html']}))
+//         .pipe(dest('css'))
+// }
+
+
+
+// function watchTask() {
+//     watch(['sabitica/**/*.scss', '*.html'], buildStyles)
+// }
+
+// I've created a folder called sass - if you were to extend Sabitica and reuse it,
+// you would simply need to copy and paste the sass in 
+// and then change 'sabitica' in the file to 'sass'
 
 function buildStyles() {
-    return src('sabitica/**/*.scss')
+    return src('sass/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({content: ['*.html']}))
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['sabitica/**/*.scss'], buildStyles)
+    watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
+
+        
 
 exports.default = series(buildStyles, watchTask);
